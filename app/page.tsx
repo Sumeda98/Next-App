c2ce39abc1835bd7f4d048cfc1383bc8543c05a1
Sumedha Bandarigoda <90858067+Sumeda98@users.noreply.github.com>
2024-03-16 15:13:51 +0530

    Server side component vs Client Side Component
    Server side component vs Client Side Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
-
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <h3>Hello World</h3>
+      <h4>Hello World</h4>
       <Link href="/users">Users</Link>
+      <ProductCard></ProductCard>
     </main>
   )
 }
