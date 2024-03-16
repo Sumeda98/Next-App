a2fb6ad5a47750ef689155c18b0b6fa9b640c06c
Sumedha Bandarigoda <90858067+Sumeda98@users.noreply.github.com>
2024-03-16 16:49:37 +0530

    data cashing

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,7 +7,11 @@ interface User {
 
 const UserPage = async () => {
 
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/users',
+    // {cache:'no-store'} // stop caching data
+    {next:{revalidate:10}} // get data every 1o seconds
+    );
   const users : User[] = await res.json();
 
   return (
