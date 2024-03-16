429869aadd236e602985365bffc25922488b6cc0
Sumedha Bandarigoda <90858067+Sumeda98@users.noreply.github.com>
2024-03-16 18:25:39 +0530

    TailwindCSS
    TailwindCSS

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import AddToCard from './AddToCard';
-import styles from './ProductCard.module.css';
+// import styles from './ProductCard.module.css';
 
 
 const ProductCard = () => {
-  return (
-    <div className={styles.cardContainer}>
-        <AddToCard/>
-    </div>
-  )
+    return (
+        // <div className={styles.cardContainer}>
+        <div className='p-5 bg-sky-500 text-white text-xl hover:bg-sky-600'>
+            <AddToCard />
+        </div>
+        // </div>
+    )
 }
 
 export default ProductCard
