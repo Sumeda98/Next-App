0d512415fc5060aeef2702474f8a98d601c6dbb3
Sumedha Bandarigoda <90858067+Sumeda98@users.noreply.github.com>
2024-03-16 17:01:29 +0530

    dynamic rendaring

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 
 interface User {
@@ -10,13 +11,14 @@ const UserPage = async () => {
   const res = await fetch(
     'https://jsonplaceholder.typicode.com/users',
     // {cache:'no-store'} // stop caching data
-    {next:{revalidate:10}} // get data every 1o seconds
+    //{next:{revalidate:10}} // get data every 1o seconds
     );
   const users : User[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
